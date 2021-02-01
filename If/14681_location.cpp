46e9089967725d56259113e50155025ad1772eac
unknown <suki5038@naver.com>
2021-02-02 06:36:46 +0900

    14681번 사분면 고르기

---
+++ If/14681_location.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int find_four(int X, int Y) {
+	// x좌표, y좌표 모두 양수, 음수 가정 
+	int location = (X > 0 ? (Y > 0 ? 1 : 4) : (Y > 0 ? 2 : 3));
+	
+	return location;
+}
+
+int main(void) {
+	int x = 0, y = 0; // initialize
+
+	cin >> x >> y; 
+	
+	cout << find_four(x, y);
+
+	return 0;
+}
+
