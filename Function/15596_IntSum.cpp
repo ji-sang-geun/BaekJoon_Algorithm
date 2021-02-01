9cefd6ca850fcfdc587153b79d5454d2c664c9b0
unknown <suki5038@naver.com>
2021-02-02 06:35:54 +0900

    15596번 정수 N개의 합

---
+++ Function/15596_IntSum.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+long long Sum(vector<int> &a) {
+	long long IntSum = 0; // Initialize
+
+	for (long unsigned int i = 0; i < a.size(); i++) { // Sum
+		IntSum = IntSum + a[i];
+	}
+
+	return IntSum; // return
+}
+
+// 이 부분까지가 정답으로 인정됨.
+
+int main(void) {
+	int n = 0; // Initialize
+	int number = 0;
+
+	cin >> n; // input 
+
+	vector<int> a(n); // vector's size
+
+	for (int i = 0; i < n; i++) { // push to vector
+		cin >> number;
+		a.push_back(number);
+	}
+
+	cout << (long long)Sum(a); // function call
+}
